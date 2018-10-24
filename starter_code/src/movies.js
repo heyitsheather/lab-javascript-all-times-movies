0695651692b5741f022558828479c9255036563d
Heather Richardson <heatherrichardsonphoto@gmail.com>
2018-10-24 11:51:53 +0200

    done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,8 +1,26 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
 
+//----------------------------------------------//
 
 // Get the average of all rates with 2 decimals 
+
+var ratesTotal = movie.rate.reduce(function(sum, rate){
+    return sum += movies.rate;
+    
+},0)
+
+function ratesAverage(){
+ratesTotal /movies.rate.length;
+
+
+
+return ratesAverage;
+}
+
+var decimalEater = parseFloat(ratesAverage.tofixed(2));
+
+
 
 
 // Get the average of Drama Movies
